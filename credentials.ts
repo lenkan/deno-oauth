991e0f0e8b2ac0c69ce87455d0903bc484b2dc80
lenkan <dlenksjo@gmail.com>
2020-04-28 21:59:29 +0200

    --amend

--- credentials.ts
+++ credentials.ts
@@ -1,16 +1,6 @@
 const credentialsData = JSON.parse(
   new TextDecoder().decode(Deno.readFileSync("credentials.json")),
-) as {
-  web: {
-    client_id: string;
-    project_id: string;
-    auth_uri: string;
-    token_uri: string;
-    auth_provider_x509_cert_url: string;
-    client_secret: string;
-    redirect_uris: string[];
-  };
-};
+) as CredentialsJSON;
 export interface Credentials {
   client_id: string;
   project_id: string;
